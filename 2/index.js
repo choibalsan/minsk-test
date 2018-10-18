0fc2acadb6db9ffc3af8fd9705b4b3988ca5a96a
choibalsan <davletovgazim@gmail.com>
2018-10-19 01:24:48 +0300

    fix 2nd solution according to clarification

--- 2/index.js
+++ 2/index.js
@@ -1,15 +1,9 @@
 function throttleIfMoreThanN(func, permittedPerSpan = 1, timeSpan = 1000) {
-  if (permittedPerSpan < 1) return () => {}; // since there's no sense to run func less than 1 time per frame, do nothing
-
   let runsCount = 0;
-  let lastArgs;
-  let lastThis;
 
   // wrapper func, that will be actually called
   function wrapper() {
     if (runsCount >= permittedPerSpan) {
-      lastArgs = arguments;
-      lastThis = this;
       // skip execution, if function runs reached the limit
       return;
     }
@@ -20,12 +14,6 @@ function throttleIfMoreThanN(func, permittedPerSpan = 1, timeSpan = 1000) {
 
     setTimeout(function() {
       runsCount = 0;
-      // at the end of the frame, check if there was skipped execution, call if so
-      if (lastArgs) {
-        wrapper.apply(lastThis, lastArgs);
-        lastArgs = null;
-        lastThis = null;
-      }
     }, timeSpan);
   }
 
--- 2/index.test.js
+++ 2/index.test.js
@@ -23,38 +23,22 @@ describe("throttleIfMoreThanN(f, 3, 1000)", function () {
     expect(log).toEqual("123");
   });
 
-  it("тормозит 4, 5 срабатывание до 1000мс, 5ое отработает после тика", function () {
+  it("тормозит 4, 5 срабатывание до 1000мс", function () {
     trottledFunc(4);
     trottledFunc(5);
     expect(log).toEqual("123");
 
     jasmine.clock().tick(1000);
-    expect(log).toEqual("1235");
+    expect(log).toEqual("123");
   });
 
-  it("3 вызова с промежутком меньше 1000 после выполнения отложенного запускасработают без задержки", function () {
+  it("3 вызова с промежутком меньше 1000 после пердыдущего фрейма выполнятся", function () {
     trottledFunc(6);
-    expect(log).toEqual("12356");
+    expect(log).toEqual("1236");
     jasmine.clock().tick(500);
     trottledFunc(7);
-    expect(log).toEqual("123567");
+    expect(log).toEqual("12367");
     trottledFunc(8);
-    expect(log).toEqual("1235678");
+    expect(log).toEqual("123678");
   });
-
-  it("3 вызова с промежутками до истечения 1000, после тика отработает только последний вызов", function () {
-    jasmine.clock().tick(400);
-    trottledFunc(9);
-    trottledFunc(10);
-    trottledFunc(11);
-    expect(log).toEqual("1235678");
-    jasmine.clock().tick(500);
-    expect(log).toEqual("123567811");
-  });
-
-  it("убедиться что в очереди на выполнении ничего не осталось", function () {
-    jasmine.clock().tick(1001);
-    expect(log).toEqual("123567811");
-  });
-
 });
